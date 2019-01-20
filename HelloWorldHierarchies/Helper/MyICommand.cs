d14e09f45f96c1cfb1faed9f1b001dd75522f51a
charleslagger <nvkhoeuet@gmail.com>
2019-01-20 23:05:32 +0700

    MVVM hierachies

---
+++ HelloWorldHierarchies/Helper/MyICommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HelloWorldHierarchies.Helper
+{
+    class MyICommand<T> : ICommand
+    {
+        Action<T> _TargetExecuteMethod;
+        Func<T, bool> _TargetCanExecuteMethod;
+
+        public MyICommand(Action<T> targetExecuteMethod)
+        {
+            _TargetExecuteMethod = targetExecuteMethod;
+        }
+
+        public MyICommand(Action<T> targetExecuteMethod, Func<T, bool> targetCanExecuteMethod)
+        {
+            _TargetExecuteMethod = targetExecuteMethod;
+            _TargetCanExecuteMethod = targetCanExecuteMethod;
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        #region ICommand Member
+
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        public bool CanExecute(object parameter)
+        {
+            if (_TargetCanExecuteMethod != null)
+            {
+                T tparm = (T)parameter;
+                return _TargetCanExecuteMethod(tparm);
+            }
+
+            if (_TargetExecuteMethod != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Execute(object parameter)
+        {
+            if(_TargetExecuteMethod != null)
+            {
+                _TargetExecuteMethod((T)parameter);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ HelloWorldHierarchies/MainWindowViewModel.cs
@@ -0,0 +1,49 @@
+﻿using HelloWorldHierarchies.Helper;
+using HelloWorldHierarchies.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldHierarchies
+{
+    class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+            NavCommand = new MyICommand<string>(OnNav);
+        }
+
+        private CustomerListViewModel customerListViewModel = new CustomerListViewModel();
+        private OrderViewModel orderViewModel = new OrderViewModel();
+
+        private BindableBase _currentViewModel;
+
+        public BindableBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set { SetProperty(ref _currentViewModel, value); }
+        }
+
+        public MyICommand<string> NavCommand
+        {
+            get;
+            private set;
+        }
+
+        private void OnNav(string destination)
+        {
+            switch(destination)
+            {
+                case "orders":
+                    CurrentViewModel = orderViewModel;
+                    break;
+                case "customers":
+                default:
+                    CurrentViewModel = customerListViewModel;
+                    break;
+            }
+        }
+    }
+}
